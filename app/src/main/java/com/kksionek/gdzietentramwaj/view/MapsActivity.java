27fb6116974f5fe9312e75ffaf880ccb17d969ae
Karol Ksionek <karlkar@tlen.pl>
2018-03-17 23:39:20 +0100

    Dialog contains link and shows on first launch

--- app/src/main/java/com/kksionek/gdzietentramwaj/view/MapsActivity.java
+++ app/src/main/java/com/kksionek/gdzietentramwaj/view/MapsActivity.java
@@ -6,20 +6,26 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
@@ -55,10 +61,15 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1234;
     private static final String TAG = "MAPSACTIVITY";
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1234;
+
     private static final int MAX_VISIBLE_MARKERS = 50;
     private static final double DISTANCE = 0.1 / 6371.0;
+
+    private static final int BUILD_VERSION_WELCOME_WINDOW_ADDED = 23;
+    private static final String PREF_LAST_VERSION = "LAST_VERSION";
 
     private GoogleMap mMap = null;
     private final HashMap<String, TramMarker> mTramMarkerHashMap = new HashMap<>();
@@ -205,6 +216,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 tramMarker.getPolyline().setPoints(pointsList);
             }
         });
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int lastVersion = sharedPreferences.getInt(PREF_LAST_VERSION, 0);
+        if (lastVersion < BUILD_VERSION_WELCOME_WINDOW_ADDED) {
+            showAboutAppDialog();
+        }
+        sharedPreferences.edit().putInt(PREF_LAST_VERSION, BuildConfig.VERSION_CODE).apply();
     }
 
     private static LatLng computeDistanceAndBearing(LatLng dst, LatLng src) {
@@ -286,11 +304,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_item_info) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(R.string.about_app);
-            builder.setMessage(R.string.disclaimer);
-            builder.setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss());
-            builder.show();
+            showAboutAppDialog();
             return true;
         } else if (item.getItemId() == R.id.menu_item_refresh) {
             mViewModel.forceReload();
@@ -518,6 +532,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         }
+    }
+
+    private void showAboutAppDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        TextView text = new TextView(this);
+        text.setText(R.string.disclaimer);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        FrameLayout container = new FrameLayout(this);
+        FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        int marginSize = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+        params.leftMargin = marginSize;
+        params.rightMargin = marginSize;
+        params.topMargin = marginSize;
+        params.bottomMargin = marginSize;
+        text.setLayoutParams(params);
+        container.addView(text);
+
+        builder.setTitle(R.string.about_app);
+        builder.setView(container);
+//        builder.setMessage(R.string.disclaimer);
+        builder.setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss());
+        builder.show();
     }
 
     void rateApp() {
