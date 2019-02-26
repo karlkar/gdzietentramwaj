025b0358892cd6ee27bb7433721dd78560fcc135
Karol <karlkar@tlen.pl>
2019-02-26 21:34:29 +0100

    Disable crashlytics for debug

--- app/src/main/java/com/kksionek/gdzietentramwaj/TramApplication.kt
+++ app/src/main/java/com/kksionek/gdzietentramwaj/TramApplication.kt
@@ -2,12 +2,9 @@ package com.kksionek.gdzietentramwaj
 
 import android.support.multidex.MultiDexApplication
 import android.util.Log
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.kksionek.gdzietentramwaj.di.AppComponent
 import com.kksionek.gdzietentramwaj.di.AppModule
 import com.kksionek.gdzietentramwaj.di.DaggerAppComponent
-import io.fabric.sdk.android.Fabric
 import io.reactivex.exceptions.UndeliverableException
 import io.reactivex.plugins.RxJavaPlugins
 import java.io.IOException
@@ -23,17 +20,6 @@ class TramApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        Fabric.with(
-            this,
-            Crashlytics.Builder()
-                .core(
-                    CrashlyticsCore.Builder()
-                        .disabled(BuildConfig.DEBUG)
-                        .build()
-                )
-                .build()
-        )
-
         appComponent = DaggerAppComponent
             .builder()
             .appModule(AppModule(this))
@@ -43,33 +29,33 @@ class TramApplication : MultiDexApplication() {
 
         RxJavaPlugins.setErrorHandler { e ->
             val cause = if (e is UndeliverableException) e.cause else e
-            if (cause is IOException) {
-                // fine, irrelevant network problem or API that throws on cancellation
-                return@setErrorHandler
-            }
-            if (cause is InterruptedException) {
-                // fine, some blocking code was interrupted by a dispose call
-                return@setErrorHandler
-            }
-            if (cause is NullPointerException || cause is IllegalArgumentException) {
-                // that's likely a bug in the application
-                Log.e(TAG, "UndeliverableException happened (probably bug): $cause.message}")
-                crashReportingService.reportCrash(
-                    cause,
-                    "UndeliverableException happened (probably bug)"
-                )
-                return@setErrorHandler
-            }
-            if (cause is IllegalStateException) {
-                // that's a bug in RxJava or in a custom operator
-                Log.e(
-                    TAG,
-                    "UndeliverableException happened (bug in RxJava or in a custom operator): ${cause.message}"
-                )
-                crashReportingService.reportCrash(
-                    cause,
-                    "UndeliverableException happened (bug in RxJava or in a custom operator)"
-                )
+            when (cause) {
+                is IOException ->
+                    // fine, irrelevant network problem or API that throws on cancellation
+                    return@setErrorHandler
+                is InterruptedException ->
+                    // fine, some blocking code was interrupted by a dispose call
+                    return@setErrorHandler
+                is NullPointerException, is IllegalArgumentException -> {
+                    // that's likely a bug in the application
+                    Log.e(TAG, "UndeliverableException happened (probably bug): $cause.message}")
+                    crashReportingService.reportCrash(
+                        cause,
+                        "UndeliverableException happened (probably bug)"
+                    )
+                    return@setErrorHandler
+                }
+                is IllegalStateException -> {
+                    // that's a bug in RxJava or in a custom operator
+                    Log.e(
+                        TAG,
+                        "UndeliverableException happened (bug in RxJava or in a custom operator): ${cause.message}"
+                    )
+                    crashReportingService.reportCrash(
+                        cause,
+                        "UndeliverableException happened (bug in RxJava or in a custom operator)"
+                    )
+                }
             }
         }
     }
