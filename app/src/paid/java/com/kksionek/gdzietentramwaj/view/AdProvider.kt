f2138237be233611261fa61b890083e4e6cc49bf
Karol Ksionek <Karol.Ksionek@vimn.com>
2019-03-09 22:39:29 +0100

    Fix paid version

--- app/src/paid/java/com/kksionek/gdzietentramwaj/view/AdProvider.kt
+++ app/src/paid/java/com/kksionek/gdzietentramwaj/view/AdProvider.kt
@@ -3,6 +3,7 @@ package com.kksionek.gdzietentramwaj.view
 import android.content.Context
 import android.location.Location
 import android.view.ViewGroup
+import com.kksionek.gdzietentramwaj.map.view.AdProviderInterface
 
 class AdProvider : AdProviderInterface {
     override fun initialize(context: Context, adMobAppId: String) {}
