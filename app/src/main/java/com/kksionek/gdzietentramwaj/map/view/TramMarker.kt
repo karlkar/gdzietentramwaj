bf2ad26fe6499dc1f8308a81e88c6999d6232b64
Karol <karlkar@tlen.pl>
2019-03-24 09:27:21 +0100

    Fix issue wit map immutability on Galaxy ACE

--- app/src/main/java/com/kksionek/gdzietentramwaj/map/view/TramMarker.kt
+++ app/src/main/java/com/kksionek/gdzietentramwaj/map/view/TramMarker.kt
@@ -7,7 +7,6 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.graphics.Typeface.BOLD
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.support.annotation.UiThread
 import android.support.v4.content.ContextCompat
@@ -78,16 +77,15 @@ class TramMarker @UiThread constructor(tramData: TramData) {
 
         private val mBitmaps = LruCache<String, BitmapDescriptor>(50)
 
-        fun drawableToBitmap(drawable: Drawable): Bitmap {
-            if (drawable is BitmapDrawable) {
-                return drawable.bitmap
-            }
-
-            val bitmap = Bitmap.createBitmap(
+        private fun drawableToBitmap(drawable: Drawable): Bitmap {
+            var bitmap = Bitmap.createBitmap(
                 drawable.intrinsicWidth,
                 drawable.intrinsicHeight,
                 Bitmap.Config.ARGB_8888
             )
+            if (!bitmap.isMutable) { // On Galaxy ACE immutable map is returned
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+            }
             val canvas = Canvas(bitmap)
             drawable.setBounds(0, 0, canvas.width, canvas.height)
             drawable.draw(canvas)
