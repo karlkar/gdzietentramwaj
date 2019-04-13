de5e5e3410ed08e070bc76d399c84ec4d690472a
Karol Ksionek <Karol.Ksionek@vimn.com>
2019-04-13 22:03:39 +0200

    Add todos

--- app/src/main/java/com/kksionek/gdzietentramwaj/map/dataSource/lodz/LodzDifficultiesDataSource.kt
+++ app/src/main/java/com/kksionek/gdzietentramwaj/map/dataSource/lodz/LodzDifficultiesDataSource.kt
@@ -6,7 +6,7 @@ import io.reactivex.Single
 
 class LodzDifficultiesDataSource : DifficultiesDataSource {
 
-    override fun isAvailable(): Boolean = false
+    override fun isAvailable(): Boolean = false //TODO Check if it can be implemented
 
     override fun getDifficulties(): Single<List<DifficultiesEntity>> = Single.never()
 }
--- app/src/main/java/com/kksionek/gdzietentramwaj/map/dataSource/wroclaw/MpkDifficultiesDataSource.kt
+++ app/src/main/java/com/kksionek/gdzietentramwaj/map/dataSource/wroclaw/MpkDifficultiesDataSource.kt
@@ -6,7 +6,7 @@ import io.reactivex.Single
 
 class MpkDifficultiesDataSource: DifficultiesDataSource {
 
-    override fun isAvailable(): Boolean = false
+    override fun isAvailable(): Boolean = false //TODO Check if it can be implemented
 
     override fun getDifficulties(): Single<List<DifficultiesEntity>> = Single.never()
 }
